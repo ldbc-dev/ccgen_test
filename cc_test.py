3c4df90e21406ec8de21cfa97a27011529c9195a
Ariel Duarte López <arieldl85@gmail.com>
2015-10-23 17:18:24 +0200

    test script

--- cc_test.py
+++ cc_test.py
@@ -28,26 +28,84 @@ def save_graph(graph,file_name):
   pylab.close()
   del fig
 
-
-def testCC(graph, target_cc, prob, ite = 0):
+def testCC(graph, target_cc, prob, ite = 0, plist = [], cclist = []):
 	nodes = nx.nodes(graph)
-	ite = ite + 1 
+
 	for i in nodes:
 		for j in nodes:
-			if i != j and j not in g[i]:
+			if i < j and j not in g[i]:
 				p = np.random.uniform()
-				if p >= prob:
+				ite = ite + 1 
+				if p <= prob:
 					graph.add_edge(i, j)
+					cc = nx.average_clustering(graph, count_zeros = True)	
+					plist.append(p)
+					cclist.append(cc)
+					if cc >= target_cc:
+						return (graph, ite, plist, cclist)	
 	
-	cc = nx.average_clustering(graph, count_zeros = True)	
-	if cc >= target_cc:
-		return (graph, ite)
-	else:
-		return testCC(graph, target_cc, prob, ite)
 	
+	return testCC(graph, target_cc, prob, ite, plist, cclist)
+	
+
+
+
+class ccInfo:
+	prob = None
+	estimatedCC = None
+	values = []
+
+	def __init__(self, prob, estimated, valuesList):
+		self.prob = prob
+		self.estimatedCC = estimated
+		self.values = valuesList
+
+def generateData(nClique, nObservations):
+	pInc = float(2)/float(((nClique - 1)*nClique))
+	#print pInc
+	nodes = range(0, nClique)
+	result = []
+	prob = 0
+	while prob < 1:
+		#print prob
+		cc = []
+		
+		for k in range(0, nObservations):
+			g = nx.Graph()
+			g.add_nodes_from(nodes)
+			for i in nx.nodes(g):
+				for j in nx.nodes(g):
+					if i < j and j not in g[i]:
+						p = np.random.uniform()
+						if p <= prob:
+							g.add_edge(i, j)
+			cc.append(nx.average_clustering(g, count_zeros = True))
+			#print nx.average_clustering(g, count_zeros = True)
+
+
+		estimated = ((nClique - 1) * (nClique - 2)* np.power(prob, 3))/((nClique - 1) * (nClique - 2))
+		result.append(ccInfo(prob, estimated, cc))
+		print ("%s | %s | %s" % (prob, estimated, cc))
+		prob = prob + pInc
+	return result
+			
+
+def createFile(result, fileName):
+	f = open(fileName,'w')
+	for i in result:
+		for j in i.values:
+			f.write(("%s | %s | %s\n" % (i.prob, i.estimatedCC, j)))
+	f.close()
+
+		
 
 
 if __name__ == '__main__':
+	r = generateData(10, 10)
+	createFile(r, 'data10_10.csv')
+
+
+	'''
 	nodes = range(1, 100)
 	g = nx.Graph()
 	g.add_nodes_from(nodes)
@@ -59,9 +117,12 @@ if __name__ == '__main__':
 	print 'Real cc : ' + str(cc)
 	print 'iterations: ' + str(g1[1])
 
+	print 'probs: ' + str(g1[2])
+
+	print 'cc: ' + str(g1[3])
 	save_graph(g1[0], 'test.pdf')
 
-
+	'''
 	#g = nx.
 	#
 	#print("Clustering Coefficient: "+str(nx.average_clustering(g, count_zeros=True)))
